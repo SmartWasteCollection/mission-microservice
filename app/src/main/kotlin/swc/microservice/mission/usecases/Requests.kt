eb8be1654dbfbfe2cd022da132906a2d900d68ca
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-07-01 13:00:53 +0200

    chore: refactor ktor requests

--- app/src/main/kotlin/swc/microservice/mission/usecases/Requests.kt
+++ app/src/main/kotlin/swc/microservice/mission/usecases/Requests.kt
@@ -4,44 +4,37 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.HttpRequestBuilder
-import io.ktor.client.request.headers
-import io.ktor.client.request.request
+import io.ktor.client.request.get
+import io.ktor.client.request.put
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpMethod
-import io.ktor.http.URLProtocol
 import io.ktor.http.contentType
-import io.ktor.http.path
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import swc.microservice.mission.entities.Booking
 import swc.microservice.mission.entities.BookingStatus
+import swc.microservice.mission.entities.ExtraordinaryWaste
 import swc.microservice.mission.entities.TypeOfWaste
 import swc.microservice.mission.entities.Waste
-import swc.microservice.mission.usecases.RequestBuilder.CLIENT
-import swc.microservice.mission.usecases.RequestBuilder.buildRequest
-import swc.microservice.mission.usecases.RequestBuilder.withBody
+import swc.microservice.mission.usecases.RequestBuilder.getBookingsRequest
+import swc.microservice.mission.usecases.RequestBuilder.updateBookingsRequest
 
-suspend fun <T : Waste> getPendingBookings(typeOfWaste: TypeOfWaste<T>): List<Booking<T>> =
-    CLIENT.request(buildRequest(HttpMethod.Get, service = Service.BOOKING, route = "/bookings"))
+suspend inline fun <reified T : Waste> getPendingBookings(typeOfWaste: TypeOfWaste<T>): List<Booking<T>> =
+    getBookingsRequest()
         .body<List<Booking<T>>>()
         .filter { it.typeOfWaste == typeOfWaste }
         .filter { it.status == BookingStatus.PENDING }
         .take(MAX_EXTRAORDINARY_MISSION_STEPS)
 
-suspend inline fun <reified T : Waste> updateBookings(bookings: List<Booking<T>>) {
-    bookings.map {
-        buildRequest(HttpMethod.Put, service = Service.BOOKING, route = "/bookings/${it.id}")
-            .withBody(Booking(it.id, it.typeOfWaste, it.position, BookingStatus.REQUESTED))
-    }.forEach { CLIENT.request(it) }
+suspend inline fun updateBookings(bookings: List<Booking<ExtraordinaryWaste>>) {
+    bookings.forEach {
+        updateBookingsRequest(Booking(it._id, it.typeOfWaste, it.position, BookingStatus.REQUESTED))
+    }
 }
 
-fun <T : Waste> deserialize(s: String): List<Booking<T>> = TODO()
-
 object RequestBuilder {
-    val CLIENT: HttpClient = HttpClient(CIO) {
+    private val CLIENT: HttpClient = HttpClient(CIO) {
         install(ContentNegotiation) {
             json(
                 Json {
@@ -51,37 +44,22 @@ object RequestBuilder {
             )
         }
     }
-    private const val BASE_URL: String = "http://swc/"
+    private const val HTTP: String = "http://"
     private val SERVICES: Map<Service, String> = mapOf(
-        Pair(Service.BOOKING, "booking")
+        Pair(Service.BOOKING, "booking"),
+        Pair(Service.LOCAL, "localhost:3000"),
     )
 
-    fun buildRequest(
-        httpMethod: HttpMethod,
-        baseUrl: String = BASE_URL,
-        service: Service,
-        route: String
-    ): HttpRequestBuilder {
-        val builder = HttpRequestBuilder()
-        builder.method = httpMethod
-        builder.url {
-            protocol = URLProtocol.HTTP
-            host = baseUrl
-            path(SERVICES[service] + route)
-        }
-        builder.headers {
-            append(HttpHeaders.Accept, "application/json")
-        }
-        return builder
-    }
+    suspend fun getBookingsRequest(): HttpResponse =
+        CLIENT.get("${HTTP}${SERVICES[Service.LOCAL]}/bookings")
 
-    inline fun <reified T> HttpRequestBuilder.withBody(body: T): HttpRequestBuilder {
-        this.contentType(ContentType.Application.Json)
-        this.setBody(body)
-        return this
-    }
+    suspend fun updateBookingsRequest(body: Booking<ExtraordinaryWaste>): HttpResponse =
+        CLIENT.put("${HTTP}${SERVICES[Service.LOCAL]}/bookings/${body._id}") {
+            setBody(body)
+            contentType(ContentType.Application.Json)
+        }
 }
 
 enum class Service {
-    BOOKING
+    BOOKING, LOCAL
 }
