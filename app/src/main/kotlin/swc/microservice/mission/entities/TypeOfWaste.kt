1adced4d835a7167e9b4e6fc903f05cf6a1cdb7c
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-07-01 12:59:19 +0200

    chore: add builder from string

--- app/src/main/kotlin/swc/microservice/mission/entities/TypeOfWaste.kt
+++ app/src/main/kotlin/swc/microservice/mission/entities/TypeOfWaste.kt
@@ -4,3 +4,14 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class TypeOfWaste<T : Waste>(val wasteName: T)
+
+fun String.toTypeOfWaste(): TypeOfWaste<ExtraordinaryWaste> = TypeOfWaste(
+    when (this.uppercase()) {
+        "ELECTRONICS" -> ExtraordinaryWaste.ELECTRONICS
+        "IRON" -> ExtraordinaryWaste.IRON
+        "CLOTHES" -> ExtraordinaryWaste.CLOTHES
+        "TWIGS" -> ExtraordinaryWaste.TWIGS
+        "WASTE_OIL" -> ExtraordinaryWaste.WASTE_OIL
+        else -> ExtraordinaryWaste.OTHER
+    }
+)
