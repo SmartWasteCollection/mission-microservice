b55edb51fdce2f7c1653d058b2a9cd8e15af4573
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-07-07 10:55:44 +0200

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,35 +10,25 @@ plugins {
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
-val ktorVersion: String by project
 val kotestVersion: String by project
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.google.guava:guava:31.1-jre")
-    implementation("io.ktor:ktor-client-core:$ktorVersion")
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     implementation("org.springframework.boot:spring-boot-starter-web:2.7.0")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.0")
-    implementation("io.reactivex.rxjava3:rxkotlin:3.0.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines:0.19.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.3")
+    implementation("com.azure:azure-digitaltwins-core:1.2.3")
+    implementation("com.azure:azure-identity:1.5.2")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest:kotest-property:$kotestVersion")
 }
 
 application {
-    // Define the main class for the application.
-    mainClass.set("swc.microservice.mission.AppKt")
+    mainClass.set("swc.microservice.mission.MissionMicroserviceKt")
 }
 
 jacoco {
