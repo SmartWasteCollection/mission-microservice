350aa2690952a167e031f48735a12799ee9fbca5
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-07-07 13:11:16 +0200

    chore: make return mission nullable and generic on waste

--- app/src/main/kotlin/swc/microservice/mission/usecases/CompleteMissionStep.kt
+++ app/src/main/kotlin/swc/microservice/mission/usecases/CompleteMissionStep.kt
@@ -3,6 +3,6 @@ package swc.microservice.mission.usecases
 import swc.microservice.mission.entities.Mission
 import swc.microservice.mission.entities.Waste
 
-class CompleteMissionStep<T : Waste>(private val missionId: String) : MissionUseCase<Mission<T>> {
-    override fun execute(manager: MissionManager): Mission<T> = manager.completeMissionStep(this.missionId)
+class CompleteMissionStep(private val missionId: String) : MissionUseCase<Mission<Waste>?> {
+    override fun execute(manager: MissionManager): Mission<Waste>? = manager.completeMissionStep(this.missionId)
 }
--- app/src/main/kotlin/swc/microservice/mission/usecases/events/MissionStepEvent.kt
+++ app/src/main/kotlin/swc/microservice/mission/usecases/events/MissionStepEvent.kt
@@ -5,6 +5,6 @@ import swc.microservice.mission.entities.Waste
 import swc.microservice.mission.usecases.CompleteMissionStep
 import swc.microservice.mission.usecases.MissionManager
 
-class MissionStepEvent<T : Waste>(private val missionId: String) : MissionEvent<Mission<T>> {
-    override fun handle(manager: MissionManager): Mission<T> = CompleteMissionStep<T>(this.missionId).execute(manager)
+class MissionStepEvent(private val missionId: String) : MissionEvent<Mission<Waste>?> {
+    override fun handle(manager: MissionManager): Mission<Waste>? = CompleteMissionStep(this.missionId).execute(manager)
 }
