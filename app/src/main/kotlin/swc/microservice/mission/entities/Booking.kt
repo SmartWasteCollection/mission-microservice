7fba1cfc1cdc04c44b95711c4c89e0383b770ecb
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-07-07 14:49:01 +0200

    chore: change id name

--- app/src/main/kotlin/swc/microservice/mission/entities/Booking.kt
+++ app/src/main/kotlin/swc/microservice/mission/entities/Booking.kt
@@ -1,7 +1,7 @@
 package swc.microservice.mission.entities
 
 data class Booking<out T : Waste>(
-    val _id: String,
+    val bookingId: String,
     val typeOfWaste: TypeOfWaste<T>,
     val position: Position,
     val status: BookingStatus = BookingStatus.PENDING
