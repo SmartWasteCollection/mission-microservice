5b618a30a282465989ffb60b5df2572e0a456c6e
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-07-07 11:43:14 +0200

    chore: add events and use cases for ordinary mission creation

---
+++ app/src/main/kotlin/swc/microservice/mission/usecases/ComputeOrdinaryMission.kt
@@ -0,0 +1,9 @@
+package swc.microservice.mission.usecases
+
+import swc.microservice.mission.entities.Mission
+import swc.microservice.mission.entities.OrdinaryWaste
+
+class ComputeOrdinaryMission(private val dumpsterId: String?) : MissionUseCase<Mission<OrdinaryWaste>> {
+    override fun execute(manager: MissionManager): Mission<OrdinaryWaste> =
+        manager.computeOrdinaryMission(this.dumpsterId)
+}
---
+++ app/src/main/kotlin/swc/microservice/mission/usecases/events/MissionRequestEvent.kt
@@ -0,0 +1,11 @@
+package swc.microservice.mission.usecases.events
+
+import swc.microservice.mission.entities.Mission
+import swc.microservice.mission.entities.OrdinaryWaste
+import swc.microservice.mission.usecases.ComputeOrdinaryMission
+import swc.microservice.mission.usecases.MissionManager
+
+class MissionRequestEvent(private val dumpsterId: String) : MissionEvent<Mission<OrdinaryWaste>> {
+    override fun handle(manager: MissionManager): Mission<OrdinaryWaste> =
+        ComputeOrdinaryMission(this.dumpsterId).execute(manager)
+}
