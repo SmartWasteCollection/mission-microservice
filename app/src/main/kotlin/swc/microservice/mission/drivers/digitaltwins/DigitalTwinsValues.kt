b7f367234e5174e52b468eedb8c90d6c81ebab74
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-07-07 22:07:55 +0200

    chore: create, read and delete operations on mission digital twins and relationships

--- app/src/main/kotlin/swc/microservice/mission/drivers/digitaltwins/DigitalTwinsValues.kt
+++ app/src/main/kotlin/swc/microservice/mission/drivers/digitaltwins/DigitalTwinsValues.kt
@@ -3,4 +3,7 @@ package swc.microservice.mission.drivers.digitaltwins
 object DigitalTwinsValues {
     const val MISSION_MODEL_ID: String = "dtmi:swc:Mission;1"
     const val ENDPOINT: String = "https://test-instance.api.wcus.digitaltwins.azure.net/"
+
+    const val RELATIONSHIP_MISSION_STEP = "MissionHasSteps"
+    const val RELATIONSHIP_MISSION_TRUCK = "MissionHasTruck"
 }
--- app/src/test/kotlin/swc/microservice/mission/drivers/digitaltwins/MissionDigitalTwinManagerTest.kt
+++ app/src/test/kotlin/swc/microservice/mission/drivers/digitaltwins/MissionDigitalTwinManagerTest.kt
@@ -1,4 +1,44 @@
 package swc.microservice.mission.drivers.digitaltwins
 
-class MissionDigitalTwinManagerTest {
-}
+import io.kotest.core.spec.style.FreeSpec
+import swc.microservice.mission.entities.Mission
+import swc.microservice.mission.entities.MissionStep
+import swc.microservice.mission.entities.OrdinaryWaste
+import swc.microservice.mission.entities.Position
+import swc.microservice.mission.entities.TypeOfMission
+import swc.microservice.mission.entities.TypeOfWaste
+
+class MissionDigitalTwinManagerTest : FreeSpec({
+    // val id = "MissionTest${System.currentTimeMillis()}"
+    val id = "MissionTest"
+    val manager = MissionDigitalTwinManager()
+
+    "The mission manager" - {
+        "when communicating with Azure Digital Twins" - {
+            "should create a digital twin" {
+                val mission = Mission(
+                    missionId = id,
+                    truckId = null,
+                    typeOfWaste = TypeOfWaste(OrdinaryWaste.PAPER),
+                    typeOfMission = TypeOfMission.ORDINARY,
+                    missionSteps = listOf(MissionStep("CollectionPoint0", Position(0L, 0L)))
+                )
+                val collectionPoint = Mission(
+                    missionId = "CollectionPointTestIncredibleFake",
+                    truckId = null,
+                    typeOfWaste = TypeOfWaste(OrdinaryWaste.PAPER),
+                    typeOfMission = TypeOfMission.ORDINARY,
+                    missionSteps = listOf()
+                )
+                manager.createMission(collectionPoint)
+                manager.createMission(mission)
+            }
+            "should read digital twins" {
+                manager.getMission(id)
+            }
+            "should delete digital twins" {
+                manager.deleteMission(id)
+            }
+        }
+    }
+})
