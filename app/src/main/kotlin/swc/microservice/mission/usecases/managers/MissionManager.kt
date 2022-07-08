2af72cfdc00bf4e7d9bd518376cbb3605a707d55
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-07-08 11:42:50 +0200

    chore: add get missions and assign truck to mission use cases

--- app/src/main/kotlin/swc/microservice/mission/usecases/managers/MissionManager.kt
+++ app/src/main/kotlin/swc/microservice/mission/usecases/managers/MissionManager.kt
@@ -3,6 +3,7 @@ package swc.microservice.mission.usecases.managers
 import swc.microservice.mission.entities.ExtraordinaryWaste
 import swc.microservice.mission.entities.Mission
 import swc.microservice.mission.entities.OrdinaryWaste
+import swc.microservice.mission.entities.Truck
 import swc.microservice.mission.entities.TypeOfWaste
 import swc.microservice.mission.entities.Waste
 
@@ -12,4 +13,8 @@ interface MissionManager {
     fun computeExtraordinaryMission(typeOfWaste: TypeOfWaste<ExtraordinaryWaste>): Mission<ExtraordinaryWaste>
 
     fun completeMissionStep(missionId: String): Mission<Waste>?
+
+    fun assignTruckToMission(missionId: String, truck: Truck): Mission<Waste>?
+
+    fun getMissions(): List<Mission<Waste>>
 }
