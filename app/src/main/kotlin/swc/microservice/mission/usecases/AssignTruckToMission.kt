124798b72cd9cb7cbae4246c24e58fdabf162a28
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-07-10 12:29:41 +0200

    chore: change use case parameters

--- app/src/main/kotlin/swc/microservice/mission/usecases/AssignTruckToMission.kt
+++ app/src/main/kotlin/swc/microservice/mission/usecases/AssignTruckToMission.kt
@@ -1,14 +1,13 @@
 package swc.microservice.mission.usecases
 
 import swc.microservice.mission.entities.Mission
-import swc.microservice.mission.entities.Truck
 import swc.microservice.mission.entities.TypeOfMission
 import swc.microservice.mission.entities.Waste
 import swc.microservice.mission.usecases.managers.ManagerSupplier
 
-class AssignTruckToMission(private val missionId: String, private val truck: Truck) : MissionUseCase<Mission<Waste>?> {
+class AssignTruckToMission(private val missionId: String, private val truckId: String) : MissionUseCase<Mission<Waste>?> {
     override fun execute(manager: ManagerSupplier): Mission<Waste>? =
         (manager.mission(TypeOfMission.ORDINARY).getMissionById(missionId) ?: manager.mission(TypeOfMission.EXTRAORDINARY).getMissionById(missionId))?.let {
-            manager.mission(it.typeOfMission).assignTruckToMission(it.missionId, truck.id)
+            manager.mission(it.typeOfMission).assignTruckToMission(it.missionId, truckId)
         }
 }
